e07ccfd02aa4f3a4b8a0cac94c253716b32d2dda
Hemant <hkbiet@gmail.com>
2024-03-16 19:07:02 +0530

    added functionality to recursively iterate directories and calculate total play time

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 import cv2
 import datetime
 import argparse
@@ -5,21 +7,35 @@ import argparse
 
 def initialise_args():
     parser = argparse.ArgumentParser(description='Get flag values')
-    parser.add_argument('-p', '--path', required=True, help='Path to the video file')
+    parser.add_argument('-p', '--path', required=True, help='root directory for video files and sub directories')
     args = parser.parse_args()
     video_file = args.path
     return video_file
 
 
-def get_video_duration(filename):
+def get_total_video_duration(filename):
     video = cv2.VideoCapture(filename)
     fps = video.get(cv2.CAP_PROP_FPS)
     frame_count = video.get(cv2.CAP_PROP_FRAME_COUNT)
-    seconds = frame_count / fps
+    try:
+        seconds = frame_count / fps
+    except:
+        return datetime.timedelta(seconds=0)
     video_time = datetime.timedelta(seconds=seconds)
     return video_time
 
+def iterate_directories(directory):
+    total_duration = datetime.timedelta(seconds=0)
+    for root, dirs, files in os.walk(directory):
+        print(f"Current directory: {root}")
+        print(f"Subdirectories: {dirs}")
+        print(f"Files: {files}")
+        for each_file in files:
+            print(f"Processing File: {each_file}")
+            total_duration += get_total_video_duration(root + "/" + each_file)
+        print("\n")
+    return total_duration
 
 if __name__ == '__main__':
-    video_duration = get_video_duration(initialise_args())
-    print(f"Video Duration: {video_duration}")
+    total_duration = iterate_directories(initialise_args())
+    print(f"Total Video Duration: {total_duration}")
