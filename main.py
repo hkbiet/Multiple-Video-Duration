030a0a3b94dfa2e5759677c9854a5a79a4719d5f
Hemant <hkbiet@gmail.com>
2024-03-16 22:17:08 +0530

    refactored with classes

--- main.py
+++ main.py
@@ -1,42 +1,52 @@
 import os
-
 import cv2
 import datetime
 import argparse
+
+
+class VideoDurationCalculator:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def calculate_duration(self):
+        video = cv2.VideoCapture(self.filename)
+        fps = video.get(cv2.CAP_PROP_FPS)
+        frame_count = video.get(cv2.CAP_PROP_FRAME_COUNT)
+        try:
+            seconds = frame_count / fps
+        except ZeroDivisionError:
+            return datetime.timedelta(seconds=0)
+        return datetime.timedelta(seconds=seconds)
+
+
+class DirectoryIterator:
+    def __init__(self, directory):
+        self.directory = directory
+
+    def iterate(self):
+        total_duration = datetime.timedelta(seconds=0)
+        for root, dirs, files in os.walk(self.directory):
+            print(f"Current directory: {root}")
+            print(f"Subdirectories: {dirs}")
+            print(f"Files: {files}")
+            for each_file in files:
+                print(f"Processing File: {each_file}")
+                video_file = os.path.join(root, each_file)
+                video_duration_calculator = VideoDurationCalculator(video_file)
+                total_duration += video_duration_calculator.calculate_duration()
+            print("\n")
+        return total_duration
 
 
 def initialise_args():
     parser = argparse.ArgumentParser(description='Get flag values')
     parser.add_argument('-p', '--path', required=True, help='root directory for video files and sub directories')
     args = parser.parse_args()
-    video_file = args.path
-    return video_file
-
-
-def get_total_video_duration(filename):
-    video = cv2.VideoCapture(filename)
-    fps = video.get(cv2.CAP_PROP_FPS)
-    frame_count = video.get(cv2.CAP_PROP_FRAME_COUNT)
-    try:
-        seconds = frame_count / fps
-    except:
-        return datetime.timedelta(seconds=0)
-    return datetime.timedelta(seconds=seconds)
-
-
-def iterate_directories(directory):
-    total_duration = datetime.timedelta(seconds=0)
-    for root, dirs, files in os.walk(directory):
-        print(f"Current directory: {root}")
-        print(f"Subdirectories: {dirs}")
-        print(f"Files: {files}")
-        for each_file in files:
-            print(f"Processing File: {each_file}")
-            total_duration += get_total_video_duration(os.path.join(root, each_file))
-        print("\n")
-    return total_duration
+    return args.path
 
 
 if __name__ == '__main__':
-    total_duration = iterate_directories(initialise_args())
+    directory = initialise_args()
+    directory_iterator = DirectoryIterator(directory)
+    total_duration = directory_iterator.iterate()
     print(f"Total Video Duration: {total_duration}")
